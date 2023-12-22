18c95938b6dede7f86b06cfe4c6fd214f250b86d
Taufik-H <taufikrpl1@gmail.com>
2023-12-22 23:36:45 +0700

    nav animation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import { NAVBAR_LINK } from "@/constant/content";
 import { Button } from "./ui/button";
 import Hamburger from "hamburger-react";
+import { motion, AnimatePresence } from "framer-motion";
 const Navbar = () => {
   const [isOpen, setOpen] = useState(false);
 
@@ -34,26 +35,46 @@ const Navbar = () => {
           <Hamburger toggled={isOpen} toggle={setOpen} size={20} />
         </div>
       </nav>
-      {isOpen && (
-        <ul className="flex flex-col gap-3 p-5 pt-6 lg:hidden text-center z-20  bg-slate-50 shadow-lg fixed w-full top-[60px] transition-all duration-200">
-          {NAVBAR_LINK.map((link) => (
-            <li key={link.key}>
-              <Link
-                className="text-slate-600 hover:text-red-500 transition-all ease-in-out duration-200 text-sm hover:font-medium"
-                href={`#${link.href}`}
-                onClick={() => setOpen(!isOpen)}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.ul
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.2 }}
+            className="flex flex-col gap-3 p-5 pt-6 lg:hidden text-center z-20 bg-slate-50 shadow-lg fixed w-full top-[60px]"
+          >
+            {NAVBAR_LINK.map((link) => (
+              <motion.li
+                key={link.key}
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ duration: 0.2 }}
               >
-                {link.label}
-              </Link>
-            </li>
-          ))}
-          <div className="flex flex-col ">
-            <Button className="rounded-full bg-red-500  text-white shadow-lg hover:bg-red-600 hover:text-white transition-all duration-200 ease-in-out shadow-red-100">
-              Join Komunitas
-            </Button>
-          </div>
-        </ul>
-      )}
+                <Link
+                  className="text-slate-600 hover:text-red-500 transition-all ease-in-out duration-200 text-sm hover:font-medium"
+                  href={`#${link.href}`}
+                  onClick={() => setOpen(!isOpen)}
+                >
+                  {link.label}
+                </Link>
+              </motion.li>
+            ))}
+            <motion.div
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
+              transition={{ duration: 0.2 }}
+              className="flex flex-col"
+            >
+              <Button className="rounded-full bg-red-500 text-white shadow-lg hover:bg-red-600 hover:text-white transition-all duration-200 ease-in-out shadow-red-100">
+                Join Komunitas
+              </Button>
+            </motion.div>
+          </motion.ul>
+        )}
+      </AnimatePresence>
     </>
   );
 };
